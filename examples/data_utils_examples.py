1221b6bd53a69b464a8a17f0d868268b3d20066c
luiserrador <luisppserrador@gmail.com>
2022-12-05 19:39:37 +0000

    initial commit

--- examples/data_utils_examples.py
+++ examples/data_utils_examples.py
@@ -136,6 +136,7 @@ def normalize_vol_max_min_example():
 
 
 def rand_mul_shi_vox_example():
+    """rand_mul_shi_vox example"""
 
     np.random.seed(1)
 
@@ -166,6 +167,7 @@ def rand_mul_shi_vox_example():
 
 
 def flip_vol_example():
+    """flip_vol example"""
     radius = 10
 
     xx, yy, zz = np.mgrid[:50, :50, :50]
@@ -207,6 +209,7 @@ def flip_vol_example():
 
 
 def zoom_z_example():
+    """zoom_z example"""
 
     np.random.seed(1)
 
@@ -266,13 +269,198 @@ def zoom_z_example():
     plt.show()
 
 
-if __name__ == '__main__':
-    # calc_centr_example()
-    # calc_dist_map_example()
-    # normalize_vol_std_example()
-    # normalize_vol_max_min_example()
-    # rand_mul_shi_vox_example()
-    # flip_vol_example()
-    zoom_z_example()
+def rotate3D_example():
+    """rotate3D example"""
 
+    np.random.seed(1)
+
+    radius = 20
+
+    xx, yy, zz = np.mgrid[:50, :50, :50]
+    sphere_data = (xx - 25) ** 2 + (yy - 25) ** 2 + (zz - 25) ** 2
+    sphere_data = np.logical_and(sphere_data < radius**2, sphere_data > 0).astype(np.float)
+    sphere_data[25, 25, 25] = 1
+
+    new_sphere1 = zoom_z(sphere_data, sphere_data, sphere_data)
+    new_sphere = rotate3D(new_sphere1[0], new_sphere1[0], new_sphere1[0])
+
+    x, y, z = np.where(new_sphere1[0] > 0.5)
+    new_x, new_y, new_z = np.where(new_sphere[0] > 0.5)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.scatter(x, y, z, color='red')
+    ax.axes.set_xlim3d(0, 50)
+    ax.axes.set_ylim3d(0, 50)
+    ax.axes.set_zlim3d(0, 50)
+    ax.set_title('Original volume')
+    ax.view_init(elev=180, azim=-45)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    ax1 = fig.add_subplot(1, 2, 2, projection='3d')
+    ax1.scatter(new_x, new_y, new_z, color='red')
+    ax1.axes.set_xlim3d(0, 50)
+    ax1.axes.set_ylim3d(0, 50)
+    ax1.axes.set_zlim3d(0, 50)
+    ax1.set_title('Rotated volume')
+    ax1.view_init(elev=180, azim=-45)
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_zlabel('Z')
+
+    figManager = plt.get_current_fig_manager()
+    figManager.window.showMaximized()
+    plt.show()
+
+
+def gauss_noise_example():
+    """gauss_noise example"""
+
+    np.random.seed(1)
+
+    radius = 10
+
+    xx, yy, zz = np.mgrid[:50, :50, :50]
+    sphere_data = (xx - 25) ** 2 + (yy - 25) ** 2 + (zz - 25) ** 2
+    sphere_data = np.logical_and(sphere_data < radius**2, sphere_data > 0).astype(np.float)
+    sphere_data[25, 25, 25] = 1
+    sphere_data = normalize_vol_max_min(sphere_data, np.max(sphere_data), np.min(sphere_data))
+    new_data = gauss_noise(sphere_data)
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(1, 2, 1)
+    ax.hist(sphere_data.ravel(), bins=800)
+    ax.set_title('Original data histogram')
+
+    ax1 = fig.add_subplot(1, 2, 2)
+    ax1.hist(new_data.ravel(), bins=800)
+    ax1.set_title('Noisy data histogram')
+
+    figManager = plt.get_current_fig_manager()
+    figManager.window.showMaximized()
+    plt.show()
+
+
+def gauss_blur_example():
+    """gauss_blur example"""
+
+    np.random.seed(1)
+
+    radius = 10
+
+    xx, yy, zz = np.mgrid[:50, :50, :50]
+    sphere_data = (xx - 25) ** 2 + (yy - 25) ** 2 + (zz - 25) ** 2
+    sphere_data = np.logical_and(sphere_data < radius**2, sphere_data > 0).astype(np.float)
+    sphere_data[25, 25, 25] = 1
+    sphere_data = normalize_vol_max_min(sphere_data, np.max(sphere_data), np.min(sphere_data))
+    new_data = gauss_blur(sphere_data)
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(2, 2, 1)
+    ax.hist(sphere_data.ravel(), bins=800)
+    ax.set_title('Original data histogram')
+
+    ax1 = fig.add_subplot(2, 2, 2)
+    ax1.hist(new_data.ravel(), bins=800)
+    ax1.set_title('Blurred data histogram')
+
+    ax2 = fig.add_subplot(2, 2, 3)
+    ax2.imshow(sphere_data[:, :, 25], cmap='gray')
+    ax2.set_title('Slice of original sphere')
+
+    ax3 = fig.add_subplot(2, 2, 4)
+    ax3.imshow(new_data[:, :, 25], cmap='gray')
+    ax3.set_title('Slice of blurred sphere')
+
+    figManager = plt.get_current_fig_manager()
+    figManager.window.showMaximized()
+    plt.show()
+
+
+def roll_imgs_example():
+    """clean_memory and roll_imgs examples"""
+    np.random.seed(2 ** 32 - 50)
+
+    radius = 25
+
+    xx, yy, zz = np.mgrid[:256, :256, :256]
+    sphere_data = (xx - 128) ** 2 + (yy - 128) ** 2 + (zz - 128) ** 2
+    sphere_data = np.logical_and(sphere_data < radius ** 2, sphere_data > (radius // 2) ** 2).astype(np.float)
+    memory = (xx - 128) ** 2 + (yy - 128) ** 2 + (zz - 64) ** 2
+    memory = np.logical_and(memory < radius ** 2, memory > (radius // 2) ** 2).astype(np.float)
+    memory_show = memory[128-64:128+64, 128-64:128+64, 128-64:128+64]
+    nb_1s = len(np.where(sphere_data == 1)[0])
+
+    new_data = roll_imgs(sphere_data, memory, sphere_data, [64, 64, 64], nb_1s)
+    x1, y1, z1 = np.where(new_data[2] == 1)
+    mem_x1, mem_y1, mem_z1 = np.where(new_data[1] == 1)
+
+    memory = clean_memory(memory)
+
+    new_data = roll_imgs(sphere_data, memory, sphere_data, [64, 64, 64], nb_1s)
+    x2, y2, z2 = np.where(new_data[2] == 1)
+    mem_x2, mem_y2, mem_z2 = np.where(new_data[1] == 1)
+
+    sphere_data_show = sphere_data[128-64:128+64, 128-64:128+64, 128-64:128+64]
+    x, y, z = np.where(sphere_data_show == 1)
+    mem_x, mem_y, mem_z = np.where(memory_show == 1)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(2, 2, 1, projection='3d')
+    ax.scatter(x, y, z, color='red')
+    ax.scatter(mem_x, mem_y, mem_z, color='blue')
+    ax.axes.set_xlim3d(0, 128)
+    ax.axes.set_ylim3d(0, 128)
+    ax.axes.set_zlim3d(0, 128)
+    ax.set_title('Original volume')
+    # ax.view_init(elev=180, azim=-45)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    ax1 = fig.add_subplot(2, 2, 2, projection='3d')
+    ax1.scatter(x1, y1, z1, color='red')
+    ax1.scatter(mem_x1, mem_y1, mem_z1, color='blue')
+    ax1.axes.set_xlim3d(0, 128)
+    ax1.axes.set_ylim3d(0, 128)
+    ax1.axes.set_zlim3d(0, 128)
+    ax1.set_title('Rolled volume')
+    # ax1.view_init(elev=180, azim=-45)
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_zlabel('Z')
+
+    ax2 = fig.add_subplot(2, 2, 3, projection='3d')
+    ax2.scatter(x2, y2, z2, color='red')
+    ax2.scatter(mem_x2, mem_y2, mem_z2, color='blue')
+    ax2.axes.set_xlim3d(0, 128)
+    ax2.axes.set_ylim3d(0, 128)
+    ax2.axes.set_zlim3d(0, 128)
+    ax2.set_title('Rolled volume without memory')
+    # ax1.view_init(elev=180, azim=-45)
+    ax2.set_xlabel('X')
+    ax2.set_ylabel('Y')
+    ax2.set_zlabel('Z')
+
+    figManager = plt.get_current_fig_manager()
+    figManager.window.showMaximized()
+    plt.show()
+
+
+if __name__ == '__main__':
+    calc_centr_example()
+    calc_dist_map_example()
+    normalize_vol_std_example()
+    normalize_vol_max_min_example()
+    rand_mul_shi_vox_example()
+    flip_vol_example()
+    zoom_z_example()
+    rotate3D_example()
+    gauss_noise_example()
+    gauss_blur_example()
+    roll_imgs_example()
 
