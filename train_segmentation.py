9f720084143632c219e42d689f29d3b77efd02eb
luiserrador <luisppserrador@gmail.com>
2022-12-06 15:24:25 +0000

    initial commit

--- train_segmentation.py
+++ train_segmentation.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-from utils.train_segmentation_utils import DatasetHandler
-
+from utils.train_segmentation_utils import DatasetHandler, Trainer
+from ML_3D_Unet.utils.layers_func import create_unet3d
 
 if __name__ == '__main__':
 
@@ -11,3 +11,18 @@ if __name__ == '__main__':
     batch_size = 1 * tf.distribute.get_strategy().num_replicas_in_sync
     datasets = DatasetHandler(training_directory, validation_directory, training_size, validation_size, batch_size)
     train_data, valid_data = datasets.get_datasets()
+
+    with tf.distribute.get_strategy().scope():
+        model = create_unet3d([128, 128, 128, 2], n_convs=2, n_filters=[16, 32, 64, 128], ksize=[3, 3, 3],
+                              padding='same', activation='relu', pooling='max', norm='batch_norm', dropout=[0], depth=4,
+                              upsampling=True)
+    model.summary()
+
+    lr = 1e-3
+    optimizer = tf.keras.optimizers.Adam
+    model_dir = 'teacher_tf_ckpts'
+    n_epochs = 45
+
+    trainer = Trainer(model, optimizer, lr, model_dir)
+    trainer.train(train_ds=train_data, valid_ds=valid_data, train_size=training_size, validation_size=validation_size,
+                  BATCH_SIZE=batch_size, EPOCHS=45)
--- utils/train_segmentation_utils.py
+++ utils/train_segmentation_utils.py
@@ -1,6 +1,7 @@
 import os
 import nibabel as nib
 import tensorflow as tf
+import time
 
 from pathlib import Path
 from utils.data_utils import *
@@ -91,16 +92,6 @@ class DatasetHandler:
                 slice_bef[f] = 0
 
         patch, memory_save, mask_save = roll_imgs(patch, memory_save, mask_save, slice_bef, nb_1s)
-
-        ## -> SLICE 128X128X128
-
-        # patch = patch[slice_bef[0]:slice_bef[0] + 128, slice_bef[1]:slice_bef[1] + 128, slice_bef[2]:slice_bef[2] + 128]
-        # patch = np.where(patch > 1, 1, patch)
-        # patch = np.where(patch < -1, -1, patch)
-        # memory_save = memory_save[slice_bef[0]:slice_bef[0] + 128, slice_bef[1]:slice_bef[1] + 128,
-        #               slice_bef[2]:slice_bef[2] + 128]
-        # mask_save = mask_save[slice_bef[0]:slice_bef[0] + 128, slice_bef[1]:slice_bef[1] + 128,
-        #             slice_bef[2]:slice_bef[2] + 128]
 
         ## -> X = patch + memory
 
@@ -232,3 +223,212 @@ class DatasetHandler:
         # recommendation is to use a non-repeating validation dataset)
 
         return train_data_iter, valid_data_iter
+
+
+class Trainer:
+    """ Generic trainer
+
+    Parameters
+    -----------
+    model : Tensorflow Model
+        Model to train
+    optimizer : Tensorflow Optimizer
+        Optimizer to use
+    learning_rate : float
+        Learning rate
+    model_dir : str
+        Directory where to save the checkpoints / model
+    """
+
+    def __init__(self, model, optimizer, learning_rate, model_dir):
+
+        self.model = model
+
+        with tf.distribute.get_strategy().scope():
+            self.train_accuracy = tf.keras.metrics.Sum()
+            self.valid_accuracy = tf.keras.metrics.Sum()
+            self.train_loss = tf.keras.metrics.Sum()
+            self.valid_loss = tf.keras.metrics.Sum()
+
+            self.optimizer = optimizer(learning_rate=learning_rate)
+
+            ckpt = tf.train.Checkpoint(optimizer=self.optimizer, net=self.model)
+            self.manager = tf.train.CheckpointManager(ckpt, model_dir, max_to_keep=3)
+            ckpt.restore(self.manager.latest_checkpoint)
+
+        check_dir = os.path.exists(model_dir)
+        if not check_dir:
+            os.makedirs(model_dir)
+
+        self.step_dir = os.path.join(model_dir, "step.npy")
+        self.model_dir = model_dir
+
+        if self.manager.latest_checkpoint:
+            print("Restored from {}".format(self.manager.latest_checkpoint))
+            self.step = np.load(self.step_dir)
+        else:
+            print("Initializing from scratch.")
+            self.step = 0
+
+    def train(self, train_ds, valid_ds, train_size, validation_size, BATCH_SIZE, EPOCHS, save_step=1):
+        """ Train the model
+
+        Parameters
+        -----------
+        train_ds : tf.data.Dataset
+            Training dataset
+        valid_ds : tf.data.Dataset
+            Validation dataset
+        train_size : scalar
+            Size of the training dataset
+        validation_size : scalar
+            Size of the validation dataset
+        loss_fn : function
+            Loss function
+        accuracy_fn : function
+            Accuracy function
+        BATCH_SIZE : int
+            Batch size
+        EPOCHS : int
+            Number of epochs to train
+        """
+
+        self.EPOCHS = EPOCHS
+        self.BATCH_SIZE = BATCH_SIZE
+
+        with tf.distribute.get_strategy().scope():
+
+            self.loss_fn = self.weight_loss_bound
+            self.accuracy_fn = self.dice_hard_coe
+
+        self.STEPS_PER_CALL = STEPS_PER_EPOCH = train_size // self.BATCH_SIZE
+        self.VALIDATION_STEPS_PER_CALL = validation_size // self.BATCH_SIZE
+        self.epoch = self.step // STEPS_PER_EPOCH
+        epoch_steps = 0
+        epoch_start_time = time.time()
+
+        history = {'loss': [], 'val_loss': [], 'acc': [], 'val_acc': []}
+
+        train_data_iter = iter(train_ds)
+        valid_data_iter = iter(valid_ds)
+
+        if self.epoch < self.EPOCHS:
+
+            while True:
+                # run training step
+                print('\nEPOCH {:d}/{:d}'.format(self.epoch + 1, self.EPOCHS))
+                self.train_step(train_data_iter)
+                epoch_steps += self.STEPS_PER_CALL
+                self.step += self.STEPS_PER_CALL
+                print(epoch_steps, '/', STEPS_PER_EPOCH)
+
+                # validation run at the end of each epoch
+                if (self.step // STEPS_PER_EPOCH) > self.epoch:
+                    # validation run
+                    valid_epoch_steps = 0
+                    self.valid_step(valid_data_iter)
+                    valid_epoch_steps += self.VALIDATION_STEPS_PER_CALL
+
+                    # compute metrics
+                    history['acc'].append(self.train_accuracy.result().numpy() / (self.BATCH_SIZE * epoch_steps))
+                    history['val_acc'].append(
+                        self.valid_accuracy.result().numpy() / (self.BATCH_SIZE * valid_epoch_steps))
+                    history['loss'].append(self.train_loss.result().numpy() / (self.BATCH_SIZE * epoch_steps))
+                    history['val_loss'].append(self.valid_loss.result().numpy() / (self.BATCH_SIZE * valid_epoch_steps))
+
+                    # report metrics
+                    epoch_time = time.time() - epoch_start_time
+                    print('time: {:0.1f}s'.format(epoch_time),
+                          'loss: {:0.4f}'.format(history['loss'][-1]),
+                          'acc: {:0.4f}'.format(history['acc'][-1]),
+                          'val_loss: {:0.4f}'.format(history['val_loss'][-1]),
+                          'val_acc: {:0.4f}'.format(history['val_acc'][-1]))
+
+                    # save checkpoint and training_step
+                    if save_step and self.epoch % save_step == 0:
+                        model_path = (os.path.join(self.model_dir, 'model_epoch_%s.h5' % (self.epoch + 1)))
+                    self.model.save(model_path)
+                    self.manager.save()
+                    np.save(self.step_dir, self.step)
+
+                    # set up next epoch
+                    self.epoch = self.step // STEPS_PER_EPOCH
+                    epoch_steps = 0
+                    epoch_start_time = time.time()
+                    self.train_accuracy.reset_states()
+                    self.valid_accuracy.reset_states()
+                    self.valid_loss.reset_states()
+                    self.train_loss.reset_states()
+                    if self.epoch >= self.EPOCHS:
+                        print('Training done, {} epochs'.format(self.epoch))
+                        break
+        else:
+            print('\nAlready trained!')
+
+    @tf.function
+    def weight_loss_bound(self, y_true, y_pred):
+        y_true_array = tf.reshape(y_true[:, :, :, :, 0], [self.BATCH_SIZE, 128, 128, 128, 1])
+        y_true_dist_map = tf.reshape(y_true[:, :, :, :, 1], [self.BATCH_SIZE, 128, 128, 128, 1])
+        power_2 = tf.fill(y_true_array.shape, 2.0)
+        gama = tf.fill(y_true_array.shape, 8.0)
+        delta = tf.fill(y_true_array.shape, 36.0)
+        ones_matrix = tf.fill(y_true_array.shape, 1.0)
+        weight = tf.add(tf.multiply(gama, tf.exp(tf.divide(-tf.pow(y_true_dist_map, power_2), delta))), ones_matrix)
+        fp_soft = tf.reduce_sum(tf.multiply(tf.multiply(weight, (tf.subtract(ones_matrix, y_true_array))), y_pred))
+        fn_soft = tf.reduce_sum(tf.multiply(tf.multiply(weight, y_true_array), tf.subtract(ones_matrix, y_pred)))
+        Lambda_min = tf.constant([0.1], dtype=tf.float32)
+        Lambda_max = tf.constant([1.0], dtype=tf.float32)
+        decim = tf.constant([10.0], dtype=tf.float32)
+        half = tf.constant([3.0], dtype=tf.float32)  # half=3, cause i'm training 30 more epochs than expected (60->90)
+        delta = tf.divide(tf.subtract(tf.cast(self.epoch, tf.float32), tf.divide(self.EPOCHS, half)), tf.divide(self.EPOCHS, decim))
+        Lambda = tf.add(Lambda_min, tf.divide(tf.subtract(Lambda_max, Lambda_min), tf.add(Lambda_max, tf.exp(
+            tf.subtract(tf.constant([0.0], dtype=tf.float32), delta)))))
+        loss = tf.add(tf.multiply(Lambda, fp_soft), fn_soft)
+
+        return loss
+
+    @tf.function
+    def dice_hard_coe(self, y_true, y_pred):
+
+        threshold = 0.5
+        axis = (1, 2, 3)
+        smooth = 1e-5
+        y_true = tf.reshape(y_true[:, :, :, :, 0], [self.BATCH_SIZE, 128, 128, 128, 1])
+        y_true = tf.cast(y_true > threshold, dtype=tf.float32)
+        y_pred = tf.reshape(y_pred[:, :, :, :, 0], [self.BATCH_SIZE, 128, 128, 128, 1])
+        y_pred = tf.cast(y_pred > threshold, dtype=tf.float32)
+        inse = tf.reduce_sum(tf.multiply(y_pred, y_true), axis=axis)
+        l = tf.reduce_sum(y_pred, axis=axis)
+        r = tf.reduce_sum(y_true, axis=axis)
+        hard_dice = (2. * inse + smooth) / (l + r + smooth)
+        ##
+        hard_dice = tf.reduce_mean(hard_dice, name='hard_dice')
+
+        return hard_dice
+
+    @tf.function
+    def train_step(self, data_iter):
+        def train_step_fn(images, labels):
+            with tf.GradientTape() as tape:
+                probabilities = self.model(images, training=True)
+                loss = self.loss_fn(tf.cast(labels, tf.float32), probabilities)
+            grads = tape.gradient(loss, self.model.trainable_variables)
+            self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+            accuracy = self.accuracy_fn(tf.cast(labels, tf.float32), probabilities)
+            self.train_accuracy.update_state(accuracy)
+            self.train_loss.update_state(loss)
+
+        for _ in tf.range(self.STEPS_PER_CALL):
+            tf.distribute.get_strategy().run(train_step_fn, next(data_iter))
+
+    @tf.function
+    def valid_step(self, data_iter):
+        def valid_step_fn(images, labels):
+            probabilities = self.model(images, training=False)
+            loss = self.loss_fn(tf.cast(labels, tf.float32), probabilities)
+            accuracy = self.accuracy_fn(tf.cast(labels, tf.float32), probabilities)
+            self.valid_accuracy.update_state(accuracy)
+            self.valid_loss.update_state(loss)
+
+        for _ in tf.range(self.VALIDATION_STEPS_PER_CALL):
+            tf.distribute.get_strategy().run(valid_step_fn, next(data_iter))
