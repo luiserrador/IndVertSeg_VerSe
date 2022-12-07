531a5946febdbe6ec9bf192068b7f73d9fc58d9b
luiserrador <luisppserrador@gmail.com>
2022-12-07 11:40:28 +0000

    initial commit

--- get_verse_data.py
+++ get_verse_data.py
@@ -21,19 +21,19 @@ print('Downloading VerSe19 Training Data')
 wget.download('https://s3.bonescreen.de/public/VerSe-complete/dataset-verse19training.zip', out='VerSe19/Training',
               bar=bar_progress)
 
-print('Downloading VerSe19 Validation Data')
+print('\nDownloading VerSe19 Validation Data')
 wget.download('https://s3.bonescreen.de/public/VerSe-complete/dataset-verse19validation.zip', out='VerSe19/Validation',
               bar=bar_progress)
 
-print('Downloading VerSe19 Test Data')
+print('\nDownloading VerSe19 Test Data')
 wget.download('https://s3.bonescreen.de/public/VerSe-complete/dataset-verse19test.zip', out='VerSe19/Test',
               bar=bar_progress)
 
-print('Downloading VerSe20 Training Data')
+print('\nDownloading VerSe20 Training Data')
 wget.download('https://s3.bonescreen.de/public/VerSe-complete/dataset-verse20training.zip', out='VerSe20/Training',
               bar=bar_progress)
 
-print('Downloading VerSe20 Validation Data')
+print('\nDownloading VerSe20 Validation Data')
 wget.download('https://s3.bonescreen.de/public/VerSe-complete/dataset-verse20validation.zip', out='VerSe20/Validation',
               bar=bar_progress)
 
