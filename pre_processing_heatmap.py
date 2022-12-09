b7bc0a46b8e7bae89d724fa54e3bb7492e4a227b
luiserrador <luisppserrador@gmail.com>
2022-12-09 14:53:58 +0000

    initial commit

---
+++ pre_processing_heatmap.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from utils.pre_preocessing_heatmap_utils import *
+
+# Training data
+
+training_v19_raw = Path('VerSe19/dataset-verse19training/rawdata/')
+training_v20_raw = Path('VerSe20/dataset-01training/rawdata/')
+
+
+training_v19_raw = [f for f in training_v19_raw.resolve().glob('**/*.nii.gz') if f.is_file()]
+training_v20_raw = [f for f in training_v20_raw.resolve().glob('**/*.nii.gz') if f.is_file()]
+
+training_raw = training_v19_raw + training_v20_raw
+
+training_v19_points = Path('VerSe19/dataset-verse19training/derivatives/')
+training_v20_points = Path('VerSe20/dataset-01training/derivatives/')
+
+training_v19_points = [f for f in training_v19_points.resolve().glob('**/*.json') if f.is_file()]
+training_v20_points = [f for f in training_v20_points.resolve().glob('**/*.json') if f.is_file()]
+
+training_points = training_v19_points + training_v20_points
+
+save_heatmap_data_training(training_raw, training_points)
+
+# Validation Data
+
+valid_v19_raw = Path('VerSe19/dataset-verse19validation/rawdata/')
+valid_v20_raw = Path('VerSe20/dataset-02validation/rawdata/')
+
+
+valid_v19_raw = [f for f in valid_v19_raw.resolve().glob('**/*.nii.gz') if f.is_file()]
+valid_v20_raw = [f for f in valid_v20_raw.resolve().glob('**/*.nii.gz') if f.is_file()]
+
+valid_raw = valid_v19_raw + valid_v20_raw
+
+valid_v19_points = Path('VerSe19/dataset-verse19validation/derivatives/')
+valid_v20_points = Path('VerSe20/dataset-02validation/derivatives/')
+
+valid_v19_points = [f for f in valid_v19_points.resolve().glob('**/*.json') if f.is_file()]
+valid_v20_points = [f for f in valid_v20_points.resolve().glob('**/*.json') if f.is_file()]
+
+valid_points = valid_v19_points + valid_v20_points
+
+save_heatmap_data_validation(valid_raw, valid_points)
---
+++ utils/pre_preocessing_heatmap_utils.py
@@ -0,0 +1,93 @@
+import nibabel as nib
+import numpy as np
+from utils.data_utilities import reorient_to, resample_nib, load_centroids, reorient_centroids_to, rescale_centroids, save_centroids
+import skimage.transform as skTrans
+
+
+def ctd_iso_resamp(ctd_iso, img_shape):
+    for j in range(1, len(ctd_iso)):
+        ctd_iso[j][1] = ctd_iso[j][1] / img_shape[0] * 64
+        ctd_iso[j][2] = ctd_iso[j][2] / img_shape[1] * 64
+        ctd_iso[j][3] = ctd_iso[j][3] / img_shape[2] * 128
+
+    return ctd_iso
+
+
+def save_heatmap_data_training(training_raw, training_points):
+
+    save_path = 'Gaussian/Training/'
+
+    n_files = len(training_raw)
+
+    for file in range(n_files):
+
+        if file + 1 < 10:
+            nb = '00' + str(file + 1)
+
+        elif file + 1 < 100:
+            nb = '0' + str(file + 1)
+
+        else:
+            nb = str(file + 1)
+
+        print('#File: ', nb)
+
+        # load files
+        img_nib = nib.load(training_raw[file])
+        ctd_list = load_centroids(training_points[file])
+
+        # Resample and Reorient data
+        img_iso = resample_nib(img_nib, voxel_spacing=(1, 1, 1), order=3)
+        ctd_iso = rescale_centroids(ctd_list, img_nib, (1, 1, 1))
+
+        img_iso = reorient_to(img_iso, axcodes_to=('P', 'R', 'I'))
+        ctd_iso = reorient_centroids_to(ctd_iso, img_iso)
+
+        # get voxel data
+        im_np = img_iso.get_fdata()
+
+        ctd_iso = ctd_iso_resamp(ctd_iso, im_np.shape)
+        im_np = skTrans.resize(im_np, (64, 64, 128), order=3, preserve_range=True)
+
+        save_centroids(ctd_iso, save_path + 'ctd/ctd' + nb + '.json')
+        np.save(save_path + 'vol_ctd/vol' + nb + '.npy', im_np)
+
+
+def save_heatmap_data_validation(valid_raw, valid_points):
+
+    save_path = 'Gaussian/Validation/'
+
+    n_files = len(valid_raw)
+
+    for file in range(n_files):
+
+        if file + 1 < 10:
+            nb = '00' + str(file + 1)
+
+        elif file + 1 < 100:
+            nb = '0' + str(file + 1)
+
+        else:
+            nb = str(file + 1)
+
+        print('#File: ', nb)
+
+        # load files
+        img_nib = nib.load(valid_raw[file])
+        ctd_list = load_centroids(valid_points[file])
+
+        # Resample and Reorient data
+        img_iso = resample_nib(img_nib, voxel_spacing=(1, 1, 1), order=3)
+        ctd_iso = rescale_centroids(ctd_list, img_nib, (1, 1, 1))
+
+        img_iso = reorient_to(img_iso, axcodes_to=('P', 'R', 'I'))
+        ctd_iso = reorient_centroids_to(ctd_iso, img_iso)
+
+        # get voxel data
+        im_np = img_iso.get_fdata()
+
+        ctd_iso = ctd_iso_resamp(ctd_iso, im_np.shape)
+        im_np = skTrans.resize(im_np, (64, 64, 128), order=3, preserve_range=True)
+
+        save_centroids(ctd_iso, save_path + 'ctd/ctd' + nb + '.json')
+        np.save(save_path + 'vol_ctd/vol' + nb + '.npy', im_np)
